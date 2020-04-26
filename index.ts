8a2c2470ede29e2dfb3e168ea6c94e075e5e2653
jan00newbie <positiveKiddo@gmail.com>
2020-04-26 19:47:51 +0200

    base

---
+++ index.ts
@@ -0,0 +1,43 @@
+import { ApolloServer, gql } from "apollo-server";
+import * as path from "path";
+import * as fs from "fs";
+import { DIRECTIVES } from "@graphql-codegen/typescript-mongodb";
+import { Resolvers } from "./types";
+import House from "./models/House";
+import Street from "./models/Street";
+import * as mongoose from "mongoose";
+
+const typeDefs = fs.readFileSync(path.join(__dirname, "schema.graphql"));
+
+const resolvers: Resolvers = {
+  Mutation: {
+    addStreet: (_, { input }) => <any>new Street(input).save(),
+    addHouse: (_, { input }) => <any>new House(input).save(),
+  },
+  Street: {
+    id: (street) => street.id,
+    houses: async (street) =>
+      <any>House.find({ street: (street as any)._id as any }),
+  },
+  House: {
+    id: (house) => house.id,
+    street: async (house) => <any>Street.findById(house.street),
+  },
+  Query: {
+    street: (_, { id }) => <any>Street.findById(id),
+  },
+};
+
+mongoose.connect(`mongodb://localhost:27017/c8wc213k_test`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+new ApolloServer({
+  resolvers,
+  typeDefs: [DIRECTIVES, gql(`${typeDefs}`)],
+})
+  .listen()
+  .then(({ port }) => {
+    console.log(`App started at localhost:${port}`);
+  });
---
+++ types.ts
@@ -0,0 +1,271 @@
+import { GraphQLResolveInfo } from 'graphql';
+export type Maybe<T> = T | null;
+export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
+
+export type HouseDbObject = {
+  id: string,
+  number: string,
+  street?: Maybe<StreetDbObject>,
+};
+
+export type StreetDbObject = {
+  id: string,
+  name: string,
+};
+
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+
+
+
+
+
+
+
+
+export type House = {
+   __typename?: 'House';
+  id: Scalars['String'];
+  number: Scalars['String'];
+  street?: Maybe<Street>;
+};
+
+export type Street = {
+   __typename?: 'Street';
+  id: Scalars['String'];
+  name: Scalars['String'];
+  houses: Array<Maybe<House>>;
+};
+
+export type Mutation = {
+   __typename?: 'Mutation';
+  addHouse: House;
+  addStreet: Street;
+};
+
+
+export type MutationAddHouseArgs = {
+  input: AddHouseInput;
+};
+
+
+export type MutationAddStreetArgs = {
+  input: AddStreetInput;
+};
+
+export type Query = {
+   __typename?: 'Query';
+  house?: Maybe<House>;
+  street?: Maybe<Street>;
+};
+
+
+export type QueryHouseArgs = {
+  id: Scalars['String'];
+};
+
+
+export type QueryStreetArgs = {
+  id: Scalars['String'];
+};
+
+export type AddHouseInput = {
+  number: Scalars['String'];
+  street: Scalars['String'];
+};
+
+export type AddStreetInput = {
+  name: Scalars['String'];
+};
+
+export type AdditionalEntityFields = {
+  path?: Maybe<Scalars['String']>;
+  type?: Maybe<Scalars['String']>;
+};
+
+export type WithIndex<TObject> = TObject & Record<string, any>;
+export type ResolversObject<TObject> = WithIndex<TObject>;
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+}
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+}
+
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
+
+export type isTypeOfResolverFn<T = {}> = (obj: T, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = ResolversObject<{
+  String: ResolverTypeWrapper<Scalars['String']>,
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>,
+  House: ResolverTypeWrapper<House>,
+  Street: ResolverTypeWrapper<Street>,
+  Mutation: ResolverTypeWrapper<{}>,
+  Query: ResolverTypeWrapper<{}>,
+  AddHouseInput: AddHouseInput,
+  AddStreetInput: AddStreetInput,
+  AdditionalEntityFields: AdditionalEntityFields,
+}>;
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = ResolversObject<{
+  String: Scalars['String'],
+  Boolean: Scalars['Boolean'],
+  House: House,
+  Street: Street,
+  Mutation: {},
+  Query: {},
+  AddHouseInput: AddHouseInput,
+  AddStreetInput: AddStreetInput,
+  AdditionalEntityFields: AdditionalEntityFields,
+}>;
+
+export type UnionDirectiveArgs = {   discriminatorField?: Maybe<Scalars['String']>;
+  additionalFields?: Maybe<Array<Maybe<AdditionalEntityFields>>>; };
+
+export type UnionDirectiveResolver<Result, Parent, ContextType = any, Args = UnionDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type AbstractEntityDirectiveArgs = {   discriminatorField: Scalars['String'];
+  additionalFields?: Maybe<Array<Maybe<AdditionalEntityFields>>>; };
+
+export type AbstractEntityDirectiveResolver<Result, Parent, ContextType = any, Args = AbstractEntityDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type EntityDirectiveArgs = {   embedded?: Maybe<Scalars['Boolean']>;
+  additionalFields?: Maybe<Array<Maybe<AdditionalEntityFields>>>; };
+
+export type EntityDirectiveResolver<Result, Parent, ContextType = any, Args = EntityDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type ColumnDirectiveArgs = {   overrideType?: Maybe<Scalars['String']>; };
+
+export type ColumnDirectiveResolver<Result, Parent, ContextType = any, Args = ColumnDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type IdDirectiveArgs = {  };
+
+export type IdDirectiveResolver<Result, Parent, ContextType = any, Args = IdDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type LinkDirectiveArgs = {   overrideType?: Maybe<Scalars['String']>; };
+
+export type LinkDirectiveResolver<Result, Parent, ContextType = any, Args = LinkDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type EmbeddedDirectiveArgs = {  };
+
+export type EmbeddedDirectiveResolver<Result, Parent, ContextType = any, Args = EmbeddedDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type MapDirectiveArgs = {   path: Scalars['String']; };
+
+export type MapDirectiveResolver<Result, Parent, ContextType = any, Args = MapDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type HouseResolvers<ContextType = any, ParentType extends ResolversParentTypes['House'] = ResolversParentTypes['House']> = ResolversObject<{
+  id?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  number?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  street?: Resolver<Maybe<ResolversTypes['Street']>, ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+}>;
+
+export type StreetResolvers<ContextType = any, ParentType extends ResolversParentTypes['Street'] = ResolversParentTypes['Street']> = ResolversObject<{
+  id?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  houses?: Resolver<Array<Maybe<ResolversTypes['House']>>, ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+}>;
+
+export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = ResolversObject<{
+  addHouse?: Resolver<ResolversTypes['House'], ParentType, ContextType, RequireFields<MutationAddHouseArgs, 'input'>>,
+  addStreet?: Resolver<ResolversTypes['Street'], ParentType, ContextType, RequireFields<MutationAddStreetArgs, 'input'>>,
+}>;
+
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = ResolversObject<{
+  house?: Resolver<Maybe<ResolversTypes['House']>, ParentType, ContextType, RequireFields<QueryHouseArgs, 'id'>>,
+  street?: Resolver<Maybe<ResolversTypes['Street']>, ParentType, ContextType, RequireFields<QueryStreetArgs, 'id'>>,
+}>;
+
+export type Resolvers<ContextType = any> = ResolversObject<{
+  House?: HouseResolvers<ContextType>,
+  Street?: StreetResolvers<ContextType>,
+  Mutation?: MutationResolvers<ContextType>,
+  Query?: QueryResolvers<ContextType>,
+}>;
+
+
+/**
+ * @deprecated
+ * Use "Resolvers" root object instead. If you wish to get "IResolvers", add "typesPrefix: I" to your config.
+*/
+export type IResolvers<ContextType = any> = Resolvers<ContextType>;
+export type DirectiveResolvers<ContextType = any> = ResolversObject<{
+  union?: UnionDirectiveResolver<any, any, ContextType>,
+  abstractEntity?: AbstractEntityDirectiveResolver<any, any, ContextType>,
+  entity?: EntityDirectiveResolver<any, any, ContextType>,
+  column?: ColumnDirectiveResolver<any, any, ContextType>,
+  id?: IdDirectiveResolver<any, any, ContextType>,
+  link?: LinkDirectiveResolver<any, any, ContextType>,
+  embedded?: EmbeddedDirectiveResolver<any, any, ContextType>,
+  map?: MapDirectiveResolver<any, any, ContextType>,
+}>;
+
+
+/**
+* @deprecated
+* Use "DirectiveResolvers" root object instead. If you wish to get "IDirectiveResolvers", add "typesPrefix: I" to your config.
+*/
+export type IDirectiveResolvers<ContextType = any> = DirectiveResolvers<ContextType>;
